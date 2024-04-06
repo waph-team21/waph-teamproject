ea24bc2c17c3f7e51be685688932358deb47f38b
wap-ramiseja <ramiseja@mail.uc.edu>
2024-04-06 16:32:11 -0400

    databasecreation

---
+++ database/database-account.sql
@@ -0,0 +1,7 @@
+create database waph_team;
+CREATE USER 'ramiseja'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL ON waph.*TO'ramiseja'@'localhost';
+
+
+
+  
---
+++ database/database-data.sql
@@ -0,0 +1,7 @@
+create database waph;
+CREATE USER 'ramiseja'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL ON waph.*TO'ramiseja'@'localhost';
+
+
+
+  
