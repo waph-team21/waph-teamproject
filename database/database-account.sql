3b54868ceb14b7de933b1fc0a072b09ef0686baf
wap-ramiseja <ramiseja@mail.uc.edu>
2024-04-06 16:50:17 -0400

    modifieddatabase

--- database/database-account.sql
+++ database/database-account.sql
@@ -1,6 +1,6 @@
-create database waph_team;
+create database waph_team21;
 CREATE USER 'team21'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
-GRANT ALL ON waph.*TO'team21'@'localhost';
+GRANT ALL ON waph_team21.*TO'team21'@'localhost';
 
 
 
--- database/database-data.sql
+++ database/database-data.sql
@@ -1,7 +1,16 @@
-create database waph;
-CREATE USER 'ramiseja'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
-GRANT ALL ON waph.*TO'ramiseja'@'localhost';
+-- if the table exists, delete it
 
+DROP TABLE IF EXISTS users;
 
+--create a new table 
 
-  
+CREATE TABLE users(
+	username varchar(50) PRIMARY KEY,
+	password varchar(100) NOT NULL);
+	
+
+-- insert data into tables
+
+LOCK TABLES 'users' WRITE;
+INSERT INTO users(username,password) VALUES ('team21', md5('Pa$$w0rd'));
+UNLOCK TABLES;
